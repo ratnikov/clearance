2aef3a65fcd332cb27fd93f76a573dacecefcb8b
Richard Penwell <almightylinuxgod@me.com>
2010-03-13 20:18:50 -0500

    Added Engine for Rails 3

--- lib/clearance.rb
+++ lib/clearance.rb
@@ -5,3 +5,9 @@ require 'clearance/configuration'
 require 'clearance/routes'
 require 'clearance/authentication'
 require 'clearance/user'
+
+module Clearance
+  class Engine < Rails::Engine
+    engine_name :clearance
+  end
+end
